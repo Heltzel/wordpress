787be177f2683095a550e5f8f1a68d680e790b81
Heltzel <heltzel.service@hotmail.com>
2022-09-15 17:32:03 +0200

    Trait & autoloader

--- readme_dev.md
+++ readme_dev.md
@@ -2,4 +2,4 @@ http://localhost/a_youtube/wordpress/codeytech_academy/wordpress/wp-admin/
 https://developer.wordpress.org/themes/
 
 https://youtu.be/cK_wvACTGMw?list=PLD8nQCAhR3tT3ehpyOpoYeUj3KHDEVK9h
-next vid 10 : https://youtu.be/WZfLcl_0S48?list=PLD8nQCAhR3tT3ehpyOpoYeUj3KHDEVK9h&t=2
+next vid 16 : https://youtu.be/IESN8wtZtyQ?list=PLD8nQCAhR3tT3ehpyOpoYeUj3KHDEVK9h&t=1
--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -5,9 +5,15 @@
  *
  * @package My Theme
  */
+if (!defined('MYTHEME_DIR_PATH')) {
+	define('MYTHEME_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+require_once MYTHEME_DIR_PATH . '/inc/helpers/autoloader.php';
 
 function myTheme_enqueue_scripts()
 {
+
+
 	wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
 	wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
 
